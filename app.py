c5f5c4bc37e305548e3129d1050fc9d652c355c3
DaNiLo <danilo.p2391@gmail.com>
2025-08-28 19:52:35 +0200

    Create first route, style and dummy blog_posts

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blog_posts = [
+    {"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post."},
+    {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post."}
+]
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
