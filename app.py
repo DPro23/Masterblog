23952546ac51c6894a032996e491904959f08468
DaNiLo <danilo.p2391@gmail.com>
2025-08-29 11:59:08 +0200

    Add posts feature + simple nav + cleaner code

--- app.py
+++ app.py
@@ -1,21 +1,58 @@
 """Host a blog and execute basic CRUD operations."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 import json
 
 app = Flask(__name__)
-blog_posts_path = "data/blog_posts.json"
+blog_posts_path = os.path.join("data", "blog_posts.json")
 
 def get_posts():
     """Return all posts from the blog"""
-    with open(blog_posts_path, 'r') as data:
-        posts = json.load(data)
-    return posts
+    with open(blog_posts_path, 'r', encoding='UTF-8') as f_load:
+        posts = json.load(f_load)
+        return posts
+
+
+def add_post(blog_posts, new_post):
+    """Add a new post to the blog"""
+    with open(blog_posts_path, 'w', encoding='UTF-8') as f_write:
+        blog_posts.append(new_post)
+        if len(blog_posts) > 0:
+            json.dump(blog_posts, f_write, indent=4)
 
 
 @app.route('/')
 def index():
     blog_posts = get_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = get_posts()
+        last_id = 0
+        # AutoIncrement id
+        if len(blog_posts) > 0:
+            if 'id' in blog_posts[-1].keys():
+                last_id = blog_posts[-1]['id']
+
+        new_id = last_id + 1
+
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        # Add post
+        post_attributes = {
+            "id": new_id,
+            "author": author,
+            "title": title,
+            "content": content,
+        }
+        add_post(blog_posts, post_attributes)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
